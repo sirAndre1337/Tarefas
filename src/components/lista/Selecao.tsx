7b259e9e0d8c4775d5dfeccd50e46bbc488c9486
sirAndre1337 <andrelluis17@hotmail.com>
2022-08-25 11:24:11 -0300

    Componente Selecao

---
+++ src/components/lista/Selecao.tsx
@@ -0,0 +1,26 @@
+interface SelecaoProps {
+    valor: boolean;
+}
+
+export default function Selecao(props: SelecaoProps) {
+
+    const gradiente = props.valor ? 'bg-gradient-to-br from-blue-400 to-purple-500' : ''
+
+    return (
+        <div className={`flex
+         justify-center
+          items-center 
+          rounded-full 
+          cursor-pointer
+          h-7
+          w-7
+          bg-red-600
+          border
+          border-gray-400
+          ${gradiente}
+          `
+        }>
+            {props.valor ? 'x' : ''}
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,14 @@
-import { useEffect, useState } from "react"
-import Tarefa from "../model/Tarefas"
-import tarefasInicias from '../data/mock';
+import Selecao from "../components/lista/Selecao";
 
 export default function Home() {
-
-  let tarefas = tarefasInicias;
-  tarefas = tarefas.filtrarConcluidas()
-  // tarefas = tarefas.removerConcluidas()
-  tarefas = tarefas.removerFiltro();
-  tarefas = tarefas.adicionarTarefa(Tarefa.criarConcluida(5, 'Ir na academia'))
-  const tarefa3 = tarefas.itens[2];
-
-  tarefas = tarefas.modificarTarefa(tarefa3.alternarStatus());
-
-  function RenderizarTarefas() {
-    return tarefas.itens.map(tarefa => {
-      return (
-        <div key={tarefa.id}>
-          <span>{tarefa.id} |</span>
-          <span> {tarefa.descricao} |</span>
-          <span> {tarefa.concluida ? 'Concluida' : 'Ativa'}</span>
-        </div>
-      )
-    })
-  }
-
 
   return (
     <div className='flex flex-col 
     items-center justify-center h-screen
      text-white bg-gradient-to-tr
       from-purple-500 to-yellow-600' >
-      {RenderizarTarefas()}
+        <Selecao valor={true}/>
+        <Selecao valor={false}/>
     </div >
   )
 }

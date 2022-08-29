d4d7c2f8f461bc99a3c5af265b1065f67545cb57
sirAndre1337 <andrelluis17@hotmail.com>
2022-08-29 15:29:41 -0300

    Removendo dados mockados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,13 @@ import Formulario from "../components/formulario/Formulario";
 import Lista from "../components/lista/Lista";
 import Cabecalho from "../components/template/Cabecalho";
 import Conteudo from "../components/template/Conteudo";
-import tarefasIniciais from '../data/mock';
+// import tarefasIniciais from '../data/mock';  Retirando os dados mockados
 import ListaTarefas from "../model/ListaTarefas";
 import Tarefa from "../model/Tarefas";
 
 export default function Home() {
 
-  const [tarefas, setTarefas] = useState<ListaTarefas>(tarefasIniciais)
+  const [tarefas, setTarefas] = useState<ListaTarefas>(new ListaTarefas([]))
 
   function novaTarefa(novaTarefa: Tarefa) {
     setTarefas(tarefas.adicionarTarefa(novaTarefa));
